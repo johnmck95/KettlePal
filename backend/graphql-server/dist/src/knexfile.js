bffb3d462b42e1ab561fc21bdc3bf34fb05a65fa
John McKinnon <johnmckinnon@Johns-MacBook-Pro.local>
2024-04-04 21:51:30 -0700

    GQL server is grabbing from a combo of mock data and DB. Fighting with knex to setup migrations, work in progress..

---
+++ backend/graphql-server/dist/src/knexfile.js
@@ -0,0 +1,13 @@
+import knex from "knex";
+import dotenv from "dotenv";
+dotenv.config();
+const knexConfig = {
+    client: "pg",
+    connection: {
+        host: process.env.KNEX_HOST,
+        user: process.env.KNEX_USER,
+        password: process.env.KNEX_PASSWORD,
+        database: process.env.KNEX_DATABASE,
+    },
+};
+export default knex(knexConfig);
