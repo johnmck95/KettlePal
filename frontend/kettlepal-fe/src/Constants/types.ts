96f2aa9f7664d414ddfd7de93e14da99457b382a
John McKinnon <johnmckinnon@Johns-MacBook-Pro.local>
2024-04-26 14:19:06 -0700

    Add types to frontend

--- frontend/kettlepal-fe/src/Constants/types.ts
+++ frontend/kettlepal-fe/src/Constants/types.ts
@@ -36,3 +36,25 @@ export type User = {
   createdAt: number;
   // password is omitted here
 };
+
+export type Workout = {
+  uid: string;
+  userUid: string;
+  startTime: string;
+  endTime: string;
+  comment: string;
+};
+
+export type Exercise = {
+  uid: string;
+  workoutUid: string;
+  title: string;
+  weight: number;
+  weightUnit: number;
+  sets: number;
+  reps: number;
+  repsDisplay: string;
+  comment: string;
+  startTime: string;
+  endTime: string;
+};
