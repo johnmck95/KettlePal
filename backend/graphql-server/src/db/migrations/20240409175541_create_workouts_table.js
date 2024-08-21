82fbedfa36ca4c93d6d11c25c6b0dcdcb801720a
John McKinnon <johnmckinnon@Johns-MacBook-Pro.local>
2024-08-20 18:33:01 -0700

    Update migrations to include createdAt in original users table migration

--- backend/graphql-server/src/db/migrations/20240409175541_create_workouts_table.js
+++ backend/graphql-server/src/db/migrations/20240409175541_create_workouts_table.js
@@ -2,6 +2,7 @@ export async function up(knex) {
   await knex.schema.createTable("workouts", function (table) {
     table.uuid("uid").primary().defaultTo(knex.raw("uuid_generate_v4()"));
     table.uuid("userUid").notNullable().references("uid").inTable("users");
+    table.timestamp("createdAt").defaultTo(knex.fn.now()).notNullable();
     table.timestamp("startTime").defaultTo(knex.fn.now());
     table.timestamp("endTime").defaultTo(knex.fn.now());
     table.string("comment", 512);
