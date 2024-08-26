021a50a84b084fac8cbf1e72988d52deeb3d6fd0
John McKinnon <johnmckinnon@Johns-MacBook-Pro.local>
2024-08-26 16:25:12 -0700

    missed one file in the comit

--- backend/graphql-server/dist/utils/verifyWorkout.js
+++ backend/graphql-server/dist/utils/verifyWorkout.js
@@ -1,5 +1,12 @@
 export function verifyWorkout(workout) {
-    if (!workout.createdAt)
+    const { createdAt, elapsedSeconds } = workout;
+    if (!createdAt)
         return { result: false, reason: "createdAt is required" };
+    if (elapsedSeconds < 0 || typeof elapsedSeconds !== "number") {
+        return {
+            result: false,
+            reason: `Workout Elapsed Seconds must be a numerical value greater than or equal to 0.`,
+        };
+    }
     return { result: true, reason: "No workout error detected." };
 }
