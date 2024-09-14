9fea6451b778d3febc14ef747b72ebf2c5e220e5
John McKinnon <johnmckinnon@Johns-MacBook-Pro.local>
2024-09-13 18:52:21 -0700

    minor, try to trigger a rebuild

--- backend/graphql-server/src/index.ts
+++ backend/graphql-server/src/index.ts
@@ -71,7 +71,6 @@ const jwtMiddleware = async (req, res, next) => {
       .where({ uid: refreshTokenData.userUid })
       .first();
     if (!user || user.tokenCount !== refreshTokenData.tokenCount) {
-      console.log("Throwing invalid token");
       throw new Error("Invalid token");
     }
 
